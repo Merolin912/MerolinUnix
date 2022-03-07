b2d9a1034ef819201917a15af833a9a24c5b6785
Merolin Thoras <98947359+Merolin912@users.noreply.github.com>
2022-03-07 15:56:21 +0530

    Unix version

---
+++ 2a.sh
@@ -0,0 +1,30 @@
+echo -n "Enter the first file"
+read file1
+if [ -e $file1 ]
+then
+set -- `ls -dl $file1`
+file1perm=$1
+else
+echo "File doesnt exists"
+exit 
+fi
+echo -n "Enter second file"
+read file2
+if [ -e $file2 ]
+then
+set -- `ls -dl $file2`
+file2perm=$1
+else
+echo "File doesnt exists"
+exit
+fi
+if [ $file1perm = $file2perm ]
+then
+echo "File permissions are identical"
+echo "Permission is $file1perm"
+else
+echo "File permissions are not identical"
+echo "$file1 permission is $file1perm"
+echo "$file2 permission is $file2perm"
+fi
+
