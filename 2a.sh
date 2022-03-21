4a4a3591439e89f7925e43ac3e85e915db5a2651
Merolin Thoras <98947359+Merolin912@users.noreply.github.com>
2022-03-21 15:59:32 +0530

    2a.sh

--- 2a.sh
+++ 2a.sh
@@ -1,3 +1,6 @@
+#Write a shell script that accepts two filenames as arguments, checks If the permissions For
+#these files are identical and If the permissions are identical, output common permissions
+#otherwise output each filename followed by its permissions.
 echo -n "Enter the first file"
 read file1
 if [ -e $file1 ]
